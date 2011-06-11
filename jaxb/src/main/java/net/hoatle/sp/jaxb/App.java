d16d71743ae12886d7ff3af64fa35ec86068a6c8
Hoat Le <hoatlevan@gmail.com>
2011-06-11 20:49:55 +0700

    first commit for jaxb sample project

---
+++ jaxb/src/main/java/net/hoatle/sp/jaxb/App.java
@@ -0,0 +1,13 @@
+package net.hoatle.sp.jaxb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
